2210eed09b71a431b03a9d5b6b904c0f45711967
Dino Stampella <dino.stampella96@gmail.com>
2024-02-03 15:09:17 -0300

    RNIC-W03: Task list

--- src/components/taskList/index.tsx
+++ src/components/taskList/index.tsx
@@ -1,43 +1,122 @@
-import React from 'react';
-import {FlatList, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import TaskCard, {type TaskCardProps} from '../taskCard';
 
 function TaskList(): React.JSX.Element {
-  const tasks: TaskCardProps[] = [
-    {title: 'Cocinar', description: 'Milanesas con pure', state: false},
-    {title: 'Cocinar', description: 'Milanesas con pure', state: false},
-    {title: 'Cocinar', description: 'Milanesas con pure', state: false},
-    {title: 'Cocinar', description: 'Milanesas con pure', state: true},
-  ];
+  const [tasks, setTasks] = useState<TaskCardProps[]>([]);
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  const onPress = (titleValue: string, descriptionValue: string) => {
+    setTitle('');
+    setDescription('');
+    Keyboard.dismiss();
+    if (!titleValue) {
+      return;
+    }
+    setTasks([
+      ...tasks,
+      {title: titleValue, description: descriptionValue, state: false},
+    ]);
+  };
+
   return (
-    <FlatList
-      style={styles.wrapper}
-      data={tasks}
-      renderItem={item => {
-        return (
-          <TaskCard
-            title={item.item.title}
-            description={item.item.description}
-            state={item.item.state}
+    <SafeAreaView style={styles.wrapper}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.wrapper}>
+        <Text style={styles.title}>Lista de tareas</Text>
+        <FlatList
+          style={styles.taskList}
+          data={tasks}
+          ListEmptyComponent={
+            <Text style={styles.listEmptyComponent}>
+              No hay tareas pendiente
+            </Text>
+          }
+          renderItem={item => {
+            return (
+              <TaskCard
+                title={item.item.title}
+                description={item.item.description}
+                state={item.item.state}
+              />
+            );
+          }}
+        />
+        <View style={styles.form}>
+          <TextInput
+            style={styles.titleInput}
+            placeholder="Título"
+            onChangeText={setTitle}
+            value={title}
           />
-        );
-      }}
-    />
+          <TextInput
+            style={styles.descriptionInput}
+            placeholder="Descripción"
+            onChangeText={setDescription}
+            value={description}
+          />
+          <TouchableOpacity
+            style={styles.touchable}
+            onPress={() => onPress(title, description)}>
+            <Text>Create task</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
+    justifyContent: 'space-between',
   },
   title: {
-    flex: 1,
+    fontSize: 40,
+    textAlign: 'center',
+    margin: 10,
   },
-  description: {
-    flex: 1,
+  taskList: {flex: 1},
+  listEmptyComponent: {alignSelf: 'center', height: '100%'},
+  form: {padding: 10, alignItems: 'center'},
+  titleInput: {
+    height: 50,
+    width: '100%',
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    borderRadius: 10,
   },
-  status: {
-    flex: 1,
+  descriptionInput: {
+    height: 50,
+    width: '100%',
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    borderRadius: 10,
+  },
+  touchable: {
+    height: 50,
+    width: '60%',
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
   },
 });
 
