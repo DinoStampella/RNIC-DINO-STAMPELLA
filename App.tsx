d6d76d849b64172fbfd9368a463ff59d07ef707e
Dino Stampella <dino.stampella96@gmail.com>
2024-02-11 12:11:37 -0300

    RNIC-W04: Splash screen and icons

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TaskList from './src/components/taskList';
+import RNBootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
   return <TaskList />;
 }
 
